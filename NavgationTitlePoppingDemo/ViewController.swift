3bdecb8ec704d8b775763ba90f1780b914337fec
lili <ll2493@163.com>
2018-05-12 17:22:40 +0800

    Initial Commit

---
+++ NavgationTitlePoppingDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Created by lili on 2018/5/12.
+//
+//  Copyright © 2018年 fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
